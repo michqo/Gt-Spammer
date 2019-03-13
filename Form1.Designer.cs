f577922539a23e16821fed00549fe7242d6519ff
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2019-03-13 21:12:43 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace savedatBuilder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.intervalValue = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.bunifuSlider1 = new Bunifu.Framework.UI.BunifuSlider();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(369, 32);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MoveWindow);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Symbol", 9.2F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(9, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gt Spammer by MichalUSER#3846";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MoveWindow);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(318, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(182, 299);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 31);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Stop Spam";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.ForeColor = System.Drawing.Color.White;
+            this.checkBox2.Location = new System.Drawing.Point(222, 179);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(109, 21);
+            this.checkBox2.TabIndex = 17;
+            this.checkBox2.Text = "Auto Interval";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.CheckBox2_CheckedChanged);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(12, 299);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 31);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Start Spam";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.8F);
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(13, 63);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(328, 100);
+            this.textBox1.TabIndex = 10;
+            // 
+            // intervalValue
+            // 
+            this.intervalValue.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+            this.intervalValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.8F);
+            this.intervalValue.ForeColor = System.Drawing.Color.White;
+            this.intervalValue.Location = new System.Drawing.Point(12, 200);
+            this.intervalValue.Name = "intervalValue";
+            this.intervalValue.Size = new System.Drawing.Size(92, 24);
+            this.intervalValue.TabIndex = 15;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(222, 206);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(115, 21);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Disable slider";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(10, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Interval (In Milliseconds):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(9, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Here enter text you want to spam:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // bunifuSlider1
+            // 
+            this.bunifuSlider1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuSlider1.BackgroudColor = System.Drawing.Color.DarkGray;
+            this.bunifuSlider1.BorderRadius = 0;
+            this.bunifuSlider1.IndicatorColor = System.Drawing.Color.SeaGreen;
+            this.bunifuSlider1.Location = new System.Drawing.Point(9, 228);
+            this.bunifuSlider1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bunifuSlider1.MaximumValue = 10000;
+            this.bunifuSlider1.Name = "bunifuSlider1";
+            this.bunifuSlider1.Size = new System.Drawing.Size(328, 35);
+            this.bunifuSlider1.TabIndex = 18;
+            this.bunifuSlider1.Value = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(243, 338);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 17);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "with help of RageX";
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.Timer3_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(12, 268);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(178, 17);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Process (Enter app name):";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.8F);
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(193, 263);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(143, 24);
+            this.textBox2.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(369, 357);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.bunifuSlider1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.intervalValue);
+            this.Controls.Add(this.checkBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Spammer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox intervalValue;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private Bunifu.Framework.UI.BunifuSlider bunifuSlider1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// TODO fix error handling, i was lazy to add it to here, better don't use try and catch, it can slow down this app.
+
+namespace savedatBuilder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public const uint WM_KEYDOWN = 0x100;
+        public const uint WM_KEYUP = 0x0101;
+        public TypeConverter converter = TypeDescriptor.GetConverter(typeof(Keys));
+        ListViewItem lv;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr Hwnd, int msg, int param, int lparam);
+
+        [DllImport("user32.dll")]
+        public static extern int ReleaseCapture();
+
+        private void MoveWindow(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, 0xA1, 0x2, 0);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            // Checking if some updates are available
+            WebClient webClient = new WebClient();
+            int crntVersion = 2988; // Current growtopia version, it's for checking if some updates are available
+            string version = webClient.DownloadString("https://gist.githubusercontent.com/MichalUSER/5c323c7d4f60516f1708f0243b4fbc5d/raw/92df077c92241c2f5d40323fcc3fe01c091ef22a/gtVersion.txt");
+            string downloadLink = webClient.DownloadString("https://gist.githubusercontent.com/MichalUSER/622cf8db4d11f35e75cac4f48088df37/raw/d72aab3cde51372be3bc1ee6ba6323cd48de5fa6/spammerDownloadlink.txt");
+            Int32.TryParse(version, out int intVersion);
+            if (crntVersion != intVersion)
+            {
+                if (MessageBox.Show("Updates are available, do you want to download them? Without update you can't use this application.", "Spammer Updater", MessageBoxButtons.YesNo) == DialogResult.Yes) // Updates are available
+                {
+                    Process.Start(downloadLink); // Start download link of that update
+                    this.Close();
+                }
+                else // Updates are not available
+                {
+                    this.Close(); // Exit the app
+                }
+            }
+            else
+            {
+                // nothing
+            }
+        }
+
+        // Exit the app
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Spam function
+        public void Spam(IntPtr Handle, string Text)
+        {
+            PostMessage(Handle, WM_KEYDOWN, (IntPtr)(Keys.Enter), IntPtr.Zero);
+            PostMessage(Handle, WM_KEYUP, (IntPtr)(Keys.Enter), IntPtr.Zero);
+            for (int i = 0; i < Text.Length; i++)
+            {
+                Keys keys1;
+                switch (Text[i].ToString())
+                {
+                    case " ":
+                        keys1 = Keys.Space;
+                        break;
+                    case "`":
+                        keys1 = (Keys)0xC0;
+                        break;
+                    case "/":
+                        keys1 = (Keys)0xBF;
+                        break;
+                    case @"":
+                        keys1 = (Keys)0xDC;
+                        break;
+                    case "[":
+                        keys1 = (Keys)0xDB;
+                        break;
+                    case "]":
+                        keys1 = (Keys)0xDD;
+                        break;
+                    case "\t":
+                    case "\n":
+                        keys1 = Keys.Space;
+                        break;
+                    default:
+                        keys1 = (Keys)converter.ConvertFromString(Text[i].ToString());
+                        break;
+                }
+                PostMessage(Handle, WM_KEYDOWN, (IntPtr)keys1, IntPtr.Zero);
+                PostMessage(Handle, WM_KEYUP, (IntPtr)keys1, IntPtr.Zero);
+            }
+            PostMessage(Handle, WM_KEYDOWN, (IntPtr)Keys.Enter, IntPtr.Zero);
+            PostMessage(Handle, WM_KEYUP, (IntPtr)Keys.Enter, IntPtr.Zero);
+        }
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            string windowTitle1 = textBox2.Text.ToString();
+            IntPtr winTitle = Process.GetProcessesByName(windowTitle1)[0].MainWindowHandle;
+            string spamText = textBox1.Text.ToString();
+            Spam(winTitle, spamText.ToUpper());
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            timer2.Start();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            timer2.Stop();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            intervalValue.Text = bunifuSlider1.Value.ToString();
+        }
+
+        // Auto interval
+        private void Timer3_Tick(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length < 21)
+            {
+                intervalValue.Text = "3600";
+                timer2.Interval = 3600;
+                bunifuSlider1.Value = 3600;
+                checkBox1.Checked = true;
+            }
+            else if (textBox1.Text.Length < 41)
+            {
+                intervalValue.Text = "6100";
+                timer2.Interval = 6100;
+                bunifuSlider1.Value = 6100;
+                checkBox1.Checked = true;
+            }
+            else if (textBox1.Text.Length < 71)
+            {
+                intervalValue.Text = "9700";
+                timer2.Interval = 9700;
+                bunifuSlider1.Value = 9700;
+                checkBox1.Checked = true;
+            }
+        }
+
+        private void CheckBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                timer3.Start();
+            }
+            else if (checkBox2.Checked == false)
+            {
+                timer3.Stop();
+            }
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                timer1.Stop();
+            }
+            else if (checkBox1.Checked == false)
+            {
+                timer1.Start();
+            }
+        }
+    }
+}
